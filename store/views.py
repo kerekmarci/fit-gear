2b95e1d6a4a47a04ef1b9a4623c280e7136742f5
kerekmarci <kerekmarci@gmail.com>
2021-11-08 14:38:29 +0000

    Check if product is added to cart, change the Add to Cart button accordingly, and if yes, display a View Cart button

--- store/views.py
+++ store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
+from bag.models import BagItem
+from bag.views import _bag_id
 
 
 def store(request, category_slug=None):
@@ -33,10 +35,13 @@ def product_detail(request, category_slug, product_slug):
     """
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        # in_bag is to establish whether the item has already been added to the bag
+        in_bag = BagItem.objects.filter(bag__bag_id=_bag_id(request), product=single_product).exists()
     except Exception as e:
         raise e
 
     context = {
         'single_product': single_product,
+        'in_bag': in_bag,
     }
     return render(request, 'store/product_detail.html', context)
