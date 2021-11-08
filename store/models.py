38cb069cd1275b5c037ac6724f8c5076236176a9
kerekmarci <kerekmarci@gmail.com>
2021-11-08 11:21:53 +0000

    Getting URL for individual Products

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from category.models import Category
+from django.urls import reverse
 
 
 class Product(models.Model):
@@ -13,6 +14,9 @@ class Product(models.Model):
     is_available = models.BooleanField(default=True)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
+
+    def get_url(self):
+        return reverse('product_detail', args=[self.category.slug, self.slug])
 
     def __str__(self):
         return self.product_name
