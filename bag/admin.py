9b0bd399664f2fcb501ac17958624ed6bbbde106
kerekmarci <kerekmarci@gmail.com>
2021-11-08 12:04:30 +0000

    Bag app created with Bag Item models

--- bag/admin.py
+++ bag/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Bag, BagItem
 
-# Register your models here.
+
+admin.site.register(Bag)
+admin.site.register(BagItem)
--- bag/models.py
+++ bag/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from store.models import Product
 
-# Create your models here.
+
+class Bag(models.Model):
+    bag_id = models.CharField(max_length=250, blank=True)
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.cart_id
+
+
+class BagItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    bag = models.ForeignKey(Bag, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.product
