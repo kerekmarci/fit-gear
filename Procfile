899acf1b545e6dfdfce1d6cb6228472581a0617c
kerekmarci <kerekmarci@gmail.com>
2021-11-09 14:59:16 +0000

    Procfile added and gunicorn installed

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn fit-gear.wsgi:application
--- fitgear/settings.py
+++ fitgear/settings.py
@@ -84,12 +84,17 @@ AUTH_USER_MODEL = 'accounts.Account'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASE = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
