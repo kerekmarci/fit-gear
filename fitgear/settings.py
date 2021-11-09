6d68b5c351cc988d651ec65e3cdccdff6deeb3cb
kerekmarci <kerekmarci@gmail.com>
2021-11-09 15:26:25 +0000

    Secret key added for deployment to Heroku

--- fitgear/settings.py
+++ fitgear/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5=@e=#xa-zu@)#p410!z1m1qir2wcuo=iyo_c_v6@7hrqs10u+'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['fit-gear.herokuapp.com', 'localhost']
 
