4bc7ea2a044d0d6ac156829d53bd7e55c7300f4e
kerekmarci <kerekmarci@gmail.com>
2021-11-09 21:09:16 +0000

    List display added for BagAdmin and BagItemAdmin

--- bag/admin.py
+++ bag/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 from .models import Bag, BagItem
 
 
-admin.site.register(Bag)
-admin.site.register(BagItem)
+class BagAdmin(admin.ModelAdmin):
+    list_display = (
+        'bag_id',
+        'date_added',
+    )
+
+class BagItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'product',
+        'bag',
+        'quantity',
+        'is_active',
+    )
+
+admin.site.register(Bag, BagAdmin)
+admin.site.register(BagItem, BagItemAdmin)
