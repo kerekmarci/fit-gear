2cfe738a3f5e3d98dcd0ec52ad6a16e5d0c8dee3
kerekmarci <kerekmarci@gmail.com>
2021-11-10 13:47:03 +0000

    Form clean method will validate form: whether email already exists or if the passwords match

--- accounts/forms.py
+++ accounts/forms.py
@@ -22,3 +22,17 @@ class RegistrationForm(forms.ModelForm):
         self.fields['email'].widget.attrs['placeholder'] = 'john.smith@example.com'
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
+
+    # Checking if password and confirm password fields are the same
+    # Resource used:
+    # https://www.codegrepper.com/code-examples/python/frameworks/django/how+to+set+a+validator+in+clean+method+form+django
+
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Passwords do not match."
+            )
