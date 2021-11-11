0d59fc3d98e0e6e11c69c1c678596899e82d46b6
kerekmarci <kerekmarci@gmail.com>
2021-11-11 10:59:35 +0000

    Login view created

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
 from .models import Account
-from django.contrib import messages
+from django.contrib import messages, auth
 
 
 def register(request):
@@ -34,8 +34,21 @@ def register(request):
 
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            # messages.success(request, 'You are now logged in.')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid login credentials.')
+            return redirect('login')
     return render(request, 'accounts/login.html')
 
 
 def logout(request):
-    return 
+    return
