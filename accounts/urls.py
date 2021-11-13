a859f202737e59cecef7d64d5d7712cfc11ef2f2
kerekmarci <kerekmarci@gmail.com>
2021-11-13 10:37:28 +0000

    Reset Password Validation added

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('', views.dashboard, name='dashboard'),
-    path('forgotPassword/', views.forgotPassword, name='forgotPassword')
+    path('forgotPassword/', views.forgotPassword, name='forgotPassword'),
+    path('resetpassword_validate/<uidb64>/<token>/', views.resetpassword_validate, name='resetpassword_validate'),
+    path('resetPassword/', views.resetPassword, name='resetPassword'),
 ]
