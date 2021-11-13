a4c2defe89eadb77bc47c9b7e29caff067f66cea
kerekmarci <kerekmarci@gmail.com>
2021-11-13 09:28:04 +0000

    URL safe base decode imported

--- accounts/views.py
+++ accounts/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 # Verification email
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
-from django.utils.http import urlsafe_base64_encode
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
