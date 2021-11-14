1362da6ad70db07852ab5e8fcc8e1ac1f17898f6
kerekmarci <kerekmarci@gmail.com>
2021-11-14 19:45:32 +0000

    Grouping product variations fixed for logged in users

--- bag/views.py
+++ bag/views.py
@@ -51,68 +51,128 @@ def add_to_bag(request, product_id):
     This view will add the product into the
     shopping bag in the given quantity, in the selected variation.
     """
+    current_user = request.user
     product = Product.objects.get(id=product_id)
-    product_variation = []
-    if request.method == 'POST':
-        # This will store the category and value in key-value pairs,
-        # for example color-white, size-medium;
-        for item in request.POST:
-            key = item
-            value = request.POST[key]
-            
-            try:
-                variation = Variation.objects.get(variation_category__iexact=key,
-                    variation_value__iexact=value, product=product)
-                product_variation.append(variation)
-            except:
-                pass
     
-    try:
-        # Get the Bag ID present in the session
-        bag = Bag.objects.get(bag_id=_bag_id(request))
-    except Bag.DoesNotExist:
-        bag = Bag.objects.create(
-            bag_id = _bag_id(request)
-        )
-    bag.save()
+    # If-Else whether user is authenticated or not
+    if current_user.is_authenticated:
+        product_variation = []
+        if request.method == 'POST':
+            # This will store the category and value in key-value pairs,
+            # for example color-white, size-medium;
+            for item in request.POST:
+                key = item
+                value = request.POST[key]
+                
+                try:
+                    variation = Variation.objects.get(variation_category__iexact=key,
+                        variation_value__iexact=value, product=product)
+                    product_variation.append(variation)
+                except:
+                    pass
 
-    # This section will group Bag Item variations, for example if the same
-    # size and colours are added again, it will not be a new line but will
-    # increase the quantity of the existing bag item
+        # This section will group Bag Item variations, for example if the same
+        # size and colours are added again, it will not be a new line but will
+        # increase the quantity of the existing bag item
 
-    is_bag_item_exists = BagItem.objects.filter(product=product, bag=bag).exists()
-    if is_bag_item_exists:
-        bag_item = BagItem.objects.filter(product=product, bag=bag)
-        existing_variation_list = []
-        bag_item_id = []
-        for item in bag_item:
-            existing_variation = item.variations.all()
-            existing_variation_list.append(list(existing_variation))
-            bag_item_id.append(item.id)
+        is_bag_item_exists = BagItem.objects.filter(product=product, user=current_user).exists()
+        if is_bag_item_exists:
+            bag_item = BagItem.objects.filter(product=product, user=current_user)
+            existing_variation_list = []
+            bag_item_id = []
+            for item in bag_item:
+                existing_variation = item.variations.all()
+                existing_variation_list.append(list(existing_variation))
+                bag_item_id.append(item.id)
 
-        if product_variation in existing_variation_list:
-            index = existing_variation_list.index(product_variation)
-            item_id = bag_item_id[index]
-            item = BagItem.objects.get(product=product, id=item_id)
-            item.quantity += 1
-            item.save()
+            if product_variation in existing_variation_list:
+                # Increase the bag item quantity 
+                index = existing_variation_list.index(product_variation)
+                item_id = bag_item_id[index]
+                item = BagItem.objects.get(product=product, id=item_id)
+                item.quantity += 1
+                item.save()
+            else:
+                item = BagItem.objects.create(product=product, quantity=1, user=current_user)
+                if len(product_variation) > 0:
+                    item.variations.clear()
+                    item.variations.add(*product_variation)
+                item.save()
         else:
-            item = BagItem.objects.create(product=product, quantity=1, bag=bag)
+            bag_item = BagItem.objects.create(
+                product = product,
+                quantity = 1,
+                user = current_user,
+            )
             if len(product_variation) > 0:
-                item.variations.clear()
-                item.variations.add(*product_variation)
-            item.save()
+                bag_item.variations.clear()
+                bag_item.variations.add(*product_variation)
+            bag_item.save()
+        return redirect('bag')
+
     else:
-        bag_item = BagItem.objects.create(
-            product = product,
-            quantity = 1,
-            bag = bag,
-        )
-        if len(product_variation) > 0:
-            bag_item.variations.clear()
-            bag_item.variations.add(*product_variation)
-        bag_item.save()
-    return redirect('bag')
+        product_variation = []
+        if request.method == 'POST':
+            # This will store the category and value in key-value pairs,
+            # for example color-white, size-medium;
+            for item in request.POST:
+                key = item
+                value = request.POST[key]
+                
+                try:
+                    variation = Variation.objects.get(variation_category__iexact=key,
+                        variation_value__iexact=value, product=product)
+                    product_variation.append(variation)
+                except:
+                    pass
+        
+        try:
+            # Get the Bag ID present in the session
+            bag = Bag.objects.get(bag_id=_bag_id(request))
+        except Bag.DoesNotExist:
+            bag = Bag.objects.create(
+                bag_id = _bag_id(request)
+            )
+        bag.save()
+
+        # This section will group Bag Item variations, for example if the same
+        # size and colours are added again, it will not be a new line but will
+        # increase the quantity of the existing bag item
+
+        is_bag_item_exists = BagItem.objects.filter(product=product, bag=bag).exists()
+        if is_bag_item_exists:
+            bag_item = BagItem.objects.filter(product=product, bag=bag)
+            existing_variation_list = []
+            bag_item_id = []
+            for item in bag_item:
+                existing_variation = item.variations.all()
+                existing_variation_list.append(list(existing_variation))
+                bag_item_id.append(item.id)
+
+            if product_variation in existing_variation_list:
+                # Increase the bag item quantity 
+                index = existing_variation_list.index(product_variation)
+                item_id = bag_item_id[index]
+                item = BagItem.objects.get(product=product, id=item_id)
+                item.quantity += 1
+                item.save()
+            else:
+                item = BagItem.objects.create(product=product, quantity=1, bag=bag)
+                if len(product_variation) > 0:
+                    item.variations.clear()
+                    item.variations.add(*product_variation)
+                item.save()
+        else:
+            bag_item = BagItem.objects.create(
+                product = product,
+                quantity = 1,
+                bag = bag,
+            )
+            if len(product_variation) > 0:
+                bag_item.variations.clear()
+                bag_item.variations.add(*product_variation)
+            bag_item.save()
+        return redirect('bag')
 
 
 def remove_from_bag(request, product_id, bag_item_id):
