fd9331405dcbd286895967a30529cf78cb0f1743
kerekmarci <kerekmarci@gmail.com>
2021-11-15 20:19:48 +0000

    Checkout forms and urls created

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def place_order(request):
+    current_user = request.user
+    bag_items = BagItem.objects.filter(user=current_user)
+    bag_count = bag_items.count()
+    if bag_count <= 0:
+        return redirect ('store')
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
--- fitgear/urls.py
+++ fitgear/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('store/', include('store.urls')),
     path('bag/', include('bag.urls')),
     path('accounts/', include('accounts.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
