24e7aabb103122076b09850aa7feed772fb36683
kerekmarci <kerekmarci@gmail.com>
2021-11-25 18:40:39 +0000

    Checkout view amended for payment

--- checkout/urls.py
+++ checkout/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [
-    path('place_order/', views.place_order, name='place_order'),
+urlpatterns = [    
     path('checkout', views.checkout, name='checkout'),
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -8,81 +8,12 @@ from .forms import OrderForm
 from .models import Order
 import datetime
 import stripe
+import json
 
 """
 def payments(request):
     return render(request, 'checkout/payments.html')
 """
-
-def place_order(request, total=0, quantity=0):
-    stripe_public_key = settings.STRIPE_PUBLIC_KEY
-    stripe_secret_key = settings.STRIPE_SECRET_KEY
-    current_user = request.user
-    bag_items = BagItem.objects.filter(user=current_user)
-    bag_count = bag_items.count()
-    if bag_count <= 0:
-        return redirect ('store')
-
-    grand_total = 0
-    tax = 0
-    for item in bag_items:
-        total += (item.product.price * item.quantity)
-        quantity += item.quantity
-    tax = (5 * total) / 100
-    grand_total = total + tax
-    stripe_total = round(grand_total * 100)
-    
-    if request.method == 'POST':
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            data = Order()
-            data.user = current_user
-            data.first_name = form.cleaned_data['first_name']
-            data.last_name = form.cleaned_data['last_name']
-            data.phone = form.cleaned_data['phone']
-            data.email = form.cleaned_data['email']
-            data.address_line_1 = form.cleaned_data['address_line_1']
-            data.address_line_2 = form.cleaned_data['address_line_2']
-            data.country = form.cleaned_data['country']
-            data.city = form.cleaned_data['city']
-            data.order_note = form.cleaned_data['order_note']
-            data.order_total = grand_total
-            data.tax = tax
-            data.ip = request.META.get('REMOTE_ADDR')
-            data.save()
-
-            # Generating Order Number
-            yr = int(datetime.date.today().strftime('%Y'))
-            dt = int(datetime.date.today().strftime('%d'))
-            mt = int(datetime.date.today().strftime('%m'))
-            d = datetime.date(yr,mt,dt)
-            current_date = d.strftime("%Y%m%d") #20210305
-            order_number = current_date + str(data.id)
-            data.order_number = order_number
-            data.save()
-
-            # Stripe
-            stripe.api_key = stripe_secret_key
-            intent = stripe.PaymentIntent.create(
-                amount=stripe_total,
-                currency=settings.STRIPE_CURRENCY,
-            )
-
-            print(intent)
-
-            order = Order.objects.get(user=current_user, is_ordered=False, order_number=order_number)
-            context = {
-                'order': order,
-                'bag_items': bag_items,
-                'total': total,
-                'tax': tax,
-                'grand_total': grand_total,
-                'stripe_public_key': stripe_public_key,
-                'client_secret': intent.client_secret,
-            }
-            return render(request, 'checkout/payments.html', context)
-    else:
-        return redirect('checkout')
 
 
 @login_required(login_url='login')
@@ -114,27 +45,26 @@ def checkout(request, total=0, quantity=0, bag_items=None):
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order_form.save()
-            return redirect(reverse('checkout_success', args=[order.order_number]))
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
+            return redirect('store')
 
     else:
-        try:
-            tax = 0
-            grand_total = 0
-            if request.user.is_authenticated:
-                bag_items = BagItem.objects.filter(user=request.user, is_active=True)    
-            else:
-                bag = Bag.objects.get(bag_id=_bag_id(request))
-                bag_items = BagItem.objects.filter(bag=bag, is_active=True)
-            for bag_item in bag_items:
-                total += (bag_item.product.price * bag_item.quantity)
-                quantity += bag_item.quantity
-            tax = (5 * total) / 100
-            grand_total = total + tax
-        except ObjectDoesNotExist:
-                pass
-
-        order_form = OrderForm()
+        tax = 0
+        grand_total = 0
+        if request.user.is_authenticated:
+            bag_items = BagItem.objects.filter(user=request.user, is_active=True)    
+        else:
+            bag = Bag.objects.get(bag_id=_bag_id(request))
+            bag_items = BagItem.objects.filter(bag=bag, is_active=True)
+        for bag_item in bag_items:
+            total += (bag_item.product.price * bag_item.quantity)
+            quantity += bag_item.quantity
+        tax = (5 * total) / 100
+        grand_total = total + tax
         
         stripe.api_key = stripe_secret_key
         stripe_total = round(grand_total * 100)
