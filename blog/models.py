b1a548b113a9c5aa2d7c145f867ec2b0650ea32b
kerekmarci <kerekmarci@gmail.com>
2021-11-27 17:56:54 +0000

    Individual blog page created with comment section

--- blog/models.py
+++ blog/models.py
@@ -6,13 +6,22 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(Account, on_delete= models.CASCADE)
-    updated_on = models.DateTimeField(auto_now= True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
 
     class Meta:
         ordering = ['-created_on']
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_on']
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
+from .models import Post
+from .forms import CommentForm
 
 
 def blog(request):
-    return render(request, 'blog.html')
+    blogposts = Post.objects.all()
+    context = {
+        'blogposts': blogposts,
+    }
+    return render(request, 'blog.html', context)
+
+
+def blog_detail(request, slug):
+    blogposts = Post.objects.get(slug=slug)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.blogposts = blogposts
+            comment.save()
+
+            return redirect('blog_detail', slug=blogposts.slug)
+    else:
+        form = CommentForm()
+
+    context = {
+        'blogposts': blogposts,
+        'form': form,
+    }
+    return render(request, 'blog_detail.html', context)
