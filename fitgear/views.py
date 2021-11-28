2726e26f06d4805978a0eec38b93efbf4eca3515
kerekmarci <kerekmarci@gmail.com>
2021-11-28 19:34:58 +0000

    Links added to card titles on store page

--- fitgear/views.py
+++ fitgear/views.py
@@ -4,6 +4,7 @@ from store.models import Product
 def home(request):
     """ A view to return the Index page """
     products = Product.objects.all().filter(is_available=True)
+    print(products)
     context = {
         'products': products,
     }
