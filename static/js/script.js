945f172f80129b4b90f5fe2716751f94458568c3
kerekmarci <kerekmarci@gmail.com>
2021-11-28 18:40:02 +0000

    Store page customised with category selector added to the left

---
+++ static/js/script.js
@@ -0,0 +1,21 @@
+/*
+This code from:
+https://templatemo.com/tm-559-zay-shop
+*/
+
+$(document).ready(function () {
+
+    // Accordion
+    var all_panels = $('.templatemo-accordion > li > ul').hide();
+
+    $('.templatemo-accordion > li > a').click(function () {
+        console.log('Hello world!');
+        var target = $(this).next();
+        if (!target.hasClass('active')) {
+            all_panels.removeClass('active').slideUp();
+            target.addClass('active').slideDown();
+        }
+        return false;
+    });
+    // End accordion
+});
