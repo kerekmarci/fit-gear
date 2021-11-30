45ada8dd306972041f2b090e40bbf90f32e98899
kerekmarci <kerekmarci@gmail.com>
2021-11-30 12:28:50 +0000

    Comments for blog registered on admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'created_on')
     list_filter = ("author",)
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'name', 'created_on')
+    list_filter = ("post",)
   
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
