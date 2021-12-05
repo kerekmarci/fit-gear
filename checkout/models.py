0cabeaa31cc4dd446e149efb9cc1596fc4fc3444
kerekmarci <kerekmarci@gmail.com>
2021-12-05 14:37:10 +0000

    Order number lenght changed

--- checkout/models.py
+++ checkout/models.py
@@ -25,7 +25,7 @@ class Order(models.Model):
 
     user = models.ForeignKey(Account, on_delete=models.SET_NULL, null=True)
     payment = models.ForeignKey(Payment, on_delete=models.SET_NULL, blank=True, null=True)
-    order_number = models.CharField(max_length=20)
+    order_number = models.CharField(max_length=40)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     phone = models.CharField(max_length=15)
