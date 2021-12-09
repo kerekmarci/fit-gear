1678dae1959f33ece9afdfb5fd54395d858a8649
kerekmarci <kerekmarci@gmail.com>
2021-12-09 18:11:19 +0000

    403 error page added

--- fitgear/views.py
+++ fitgear/views.py
@@ -24,3 +24,8 @@ def handler404(request, exception):
 def handler500(request):
     """ A view for custom 500 error page """
     return render(request, '500.html', status=500)
+
+
+def handler403(request, exception):
+    """ A view for custom 403 error page """
+    return render(request, '403.html', status=403)
